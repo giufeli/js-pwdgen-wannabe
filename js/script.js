50720707a3a05e16c32ceddbde1cacc0d99ea941
giufeli <giuliofelicetti99@gmail.com>
2022-10-19 16:30:13 +0200

    Commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let nome = prompt('Benvenuto, quale è il tuo nome?')
+
+let cognome = prompt('E il tuo cognome?')
+
+let colore = prompt ('Ora insierisci il tuo colore preferito')
+
+let numero = 22
+
+let message = 'Accesso consentito!';
+
+document.getElementById('user').innerHTML = nome + cognome + colore + numero;
+
+document.getElementById('message').innerHTML = message;
+
+
